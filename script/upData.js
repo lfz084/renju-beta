fc3dffcb6a0d5ba6aaad709c8f0c39ef0c371ee1
163084wb@163.com <163084wb@163.com>
2024-05-28 00:21:01 +0800

    update version

--- script/upData.js
+++ script/upData.js
@@ -8,7 +8,7 @@ window.upData = window.parent.upData || (function() {
     }
     
     const keyRenjuVersion = "RENJU_APP_VERSION";
-    const scriptVersion = "v2024.26009";
+    const scriptVersion = "v2024.26010";
 	let currentVersion = localStorage.getItem(keyRenjuVersion) || scriptVersion;
 	
     let updateVersion;
