285ca661539b23e2e40033f62e353f2a8cc71060
163084wb@163.com <163084wb@163.com>
2024-05-29 09:54:11 +0800

    添加默认离线资源

--- script/upData.js
+++ script/upData.js
@@ -8,7 +8,7 @@ window.upData = window.parent.upData || (function() {
     }
     
     const keyRenjuVersion = "RENJU_APP_VERSION";
-    const scriptVersion = "v2024.27027";
+    const scriptVersion = "v2024.27028";
 	let currentVersion = localStorage.getItem(keyRenjuVersion) || scriptVersion;
 	
     let updateVersion;
