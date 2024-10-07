720170c10feec936f75ef3181af1d376f6b7c7b1
163084wb@163.com <163084wb@163.com>
2024-10-08 02:25:03 +0800

    fixed renjupuzzle bug

--- script/upData.js
+++ script/upData.js
@@ -8,7 +8,7 @@ window.upData = window.parent.upData || (function() {
     }
     
     const keyRenjuVersion = "RENJU_APP_VERSION";
-    const scriptVersion = "v2024.39";
+    const scriptVersion = "v2024.45";
 	let currentVersion = localStorage.getItem(keyRenjuVersion) || scriptVersion;
 	
     let updateVersion;
